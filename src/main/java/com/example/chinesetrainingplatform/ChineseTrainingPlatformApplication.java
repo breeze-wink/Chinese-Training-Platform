b2e149d4a0fc9ffe4f96e1f98404b7c3840322b8
zebin huang <1667535930@qq.com>
2024-09-30 18:32:08 +0800

    项目启动

---
+++ src/main/java/com/example/chinesetrainingplatform/ChineseTrainingPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.chinesetrainingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChineseTrainingPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChineseTrainingPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chinesetrainingplatform/ChineseTrainingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chinesetrainingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChineseTrainingPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
