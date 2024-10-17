8b3d39a972f88811ad86eefa808d4da18d465b9f
Aozora <2271213161@qq.com>
2024-10-18 00:30:27 +0800

    实现了老师创建班级、解散班级、从班级中移除学生、学生通过班级码加入班级

--- src/main/java/com/example/mapper/classes/ClassStudentMapper.java
+++ src/main/java/com/example/mapper/classes/ClassStudentMapper.java
@@ -22,4 +22,10 @@ public interface ClassStudentMapper {
 
     @Select("SELECT * FROM class_student WHERE student_id = #{studentId}")
     List<ClassStudent> selectByStudentId(Long studentId);
+
+    @Delete("DELETE FROM class_student WHERE class_id = #{classID}")
+    int removeClass(Long classId);
+
+    @Select("SELECT id FROM class WHERE invite_code = #{inviteCode}")
+    long selectIdByInviteCode(String inviteCode);
 }
