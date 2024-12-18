29d02e68b93b7e9c5f84c99d4e7e8c162a78cea3
Aozora <2271213161@qq.com>
2024-12-19 00:21:32 +0800

    修改老师和学生获取学生统计信息的逻辑

---
+++ src/main/java/com/example/model/view/AssignmentIdStudentIdScore.java
@@ -0,0 +1,12 @@
+package com.example.model.view;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AssignmentIdStudentIdScore {
+    private Long assignmentId;
+    private Long studentId;
+    private Integer score;
+}
